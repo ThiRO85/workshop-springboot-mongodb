de6896476d7058a8901d0a8ae77f9b40733e5200
ThiRO85 <thgr85@gmail.com>
2021-05-21 11:56:06 -0300

    Obtendo um usuario por id

--- src/main/java/com/ThiRO85/workshopmongo/services/UserService.java
+++ src/main/java/com/ThiRO85/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ThiRO85.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ThiRO85.workshopmongo.domain.User;
 import com.ThiRO85.workshopmongo.repositories.UserRepository;
+import com.ThiRO85.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll(); //Busca na biblioteca do Spring um método!
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
